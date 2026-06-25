244d2b16a6d4d49f7f86bc729039b4ca6df68d5b
HanyaMas2Biasaa <yopiansah96@gmail.com>
2026-06-25 19:27:07 +0700

    Create users.php

---
+++ users.php
@@ -0,0 +1,29 @@
+<?php
+// Folder: C:\xampp\htdocs\portal-otp\users.php
+// FILE DATABASE USER & TARGET OLT (20 TEMPLATE DATA)
+
+$user_database = [
+    // Template OLT 1 (Port 2001)
+    '082289467410' => ['nama' => 'Yopi Ansah', 'chat_id' => '8264592549', 'target' => 'http://202.10.41.185:2001/'],
+    '081234567891' => ['nama' => 'Ahmad NOC', 'chat_id' => '1111111111', 'target' => 'http://202.10.41.185:2001/'],
+    '081234567892' => ['nama' => 'Budi Teknisi', 'chat_id' => '2222222222', 'target' => 'http://202.10.41.185:2001/'],
+    '081234567893' => ['nama' => 'User Template 4', 'chat_id' => '3333333333', 'target' => 'http://202.10.41.185:2001/'],
+    '081234567894' => ['nama' => 'User Template 5', 'chat_id' => '4444444444', 'target' => 'http://202.10.41.185:2001/'],
+    '081234567895' => ['nama' => 'User Template 6', 'chat_id' => '5555555555', 'target' => 'http://202.10.41.185:2001/'],
+    '081234567896' => ['nama' => 'User Template 7', 'chat_id' => '6666666666', 'target' => 'http://202.10.41.185:2001/'],
+    '081234567897' => ['nama' => 'User Template 8', 'chat_id' => '7777777777', 'target' => 'http://202.10.41.185:2001/'],
+    '081234567898' => ['nama' => 'User Template 9', 'chat_id' => '8888888888', 'target' => 'http://202.10.41.185:2001/'],
+    '081234567899' => ['nama' => 'User Template 10', 'chat_id' => '9999999999', 'target' => 'http://202.10.41.185:2001/'],
+
+    // Template OLT 2 (Port 2002)
+    '085211112221' => ['nama' => 'User Template 11', 'chat_id' => '1212121212', 'target' => 'http://202.10.41.185:2002/'],
+    '085211112222' => ['nama' => 'User Template 12', 'chat_id' => '2323232323', 'target' => 'http://202.10.41.185:2002/'],
+    '085211112223' => ['nama' => 'User Template 13', 'chat_id' => '3434343434', 'target' => 'http://202.10.41.185:2002/'],
+    '085211112224' => ['nama' => 'User Template 14', 'chat_id' => '4545454545', 'target' => 'http://202.10.41.185:2002/'],
+    '085211112225' => ['nama' => 'User Template 15', 'chat_id' => '5656565656', 'target' => 'http://202.10.41.185:2002/'],
+    '085211112226' => ['nama' => 'User Template 16', 'chat_id' => '6767676767', 'target' => 'http://202.10.41.185:2002/'],
+    '085211112227' => ['nama' => 'User Template 17', 'chat_id' => '7878787878', 'target' => 'http://202.10.41.185:2002/'],
+    '085211112228' => ['nama' => 'User Template 18', 'chat_id' => '8989898989', 'target' => 'http://202.10.41.185:2002/'],
+    '085211112229' => ['nama' => 'User Template 19', 'chat_id' => '9090909090', 'target' => 'http://202.10.41.185:2002/'],
+    '085211112230' => ['nama' => 'User Template 20', 'chat_id' => '9898989898', 'target' => 'http://202.10.41.185:2002/'],
+];
